282404026c0cc0f770c6a62774a59f7c70116aec
Alec Reid <alecreid0093@gmail.com>
2019-09-17 21:54:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,16 +2,13 @@ class Dog
   def initialize(name, breed)
     @name = name 
     @breed = breed
-    @breed = "Mutt"
   end 
 
   def name=(new_name)
     @name = name
   end
 
-  def breed=(new_breed)
-    @breed = breed
-  end
+  
 end
 
 fido = Dog.new("Fido", "Pug")
